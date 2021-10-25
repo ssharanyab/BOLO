f5b779520056c30d5cc826f8fb8953cadfe831c3
deltarfd <deltarfd@live.com>
2021-10-25 19:14:49 +0700

    Rebase Application

--- android/app/src/main/kotlin/com/example/comhelper/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/comhelper/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.yolo
+package com.example.comhelper
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ lib/splashscreen.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'utils/deviceSize.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  VideoState createState() => VideoState();
+}
+
+DeviceSize deviceSize;
+
+class VideoState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  AnimationController animationController;
+  Animation<double> animation;
+
+  startTime() async {
+    var _duration = new Duration(seconds: 3);
+    return new Timer(_duration, navigationPage);
+  }
+
+  Future<void> navigationPage() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    Navigator.of(context).pushReplacementNamed('/home');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = new AnimationController(
+        vsync: this, duration: new Duration(seconds: 2));
+    animation =
+        new CurvedAnimation(parent: animationController, curve: Curves.easeOut);
+
+    animation.addListener(() => this.setState(() {}));
+    animationController.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    deviceSize = DeviceSize(
+        size: MediaQuery.of(context).size,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        aspectRatio: MediaQuery.of(context).size.aspectRatio);
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Image.asset(
+                'assets/images/comhelper_logo.png',
+                width: animation.value * 250,
+                height: animation.value * 250,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
